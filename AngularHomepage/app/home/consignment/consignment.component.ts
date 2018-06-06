87f465fc82a83bb79257b5a10a28f7309b444e5d
antonnystedt <32243913+antonnystedt@users.noreply.github.com>
2018-06-07 01:35:08 +0200

    New pagination completely fixed for consignment.component

--- AngularHomepage/app/home/consignment/consignment.component.ts
+++ AngularHomepage/app/home/consignment/consignment.component.ts
@@ -22,16 +22,13 @@ export class ConsignmentComponent implements OnInit {
     activeConsignments: ActiveConsignment[];
     consignments: ActiveConsignment[];
     // pager object
-    pager: any = {};
+    pager: Pager = new Pager();
 
     // paged items
     pagedItems: ActiveConsignment[];
 
     currentUser: Person;
     confirmResult: boolean = null;
-    maxSliceValue = 4;
-    minSliceValue = 0;
-    filteringSliceValue = 4;
     showAllConsignmentsEnabled = false;
     showConsignments = true;
     loading = false;
@@ -102,22 +99,6 @@ export class ConsignmentComponent implements OnInit {
         }
     }
 
-    //showOnlyArchivedConsignments(event: any) {
-    //    if (event.srcElement.checked)
-    //        this.onlyArchivedConsignments();
-    //    else
-    //        this.consignments = this.activeConsignments;
-    //}
-
-    //onlyArchivedConsignments() {
-    //    if (this.archivedConsignments != null) {
-    //        this.consignments = this.archivedConsignments;
-    //    }
-    //    else {
-    //        this.getOnlyArchivedConsignments();
-    //    }
-    //}
-
     getOnlyArchivedConsignments(){
         this.consignmentService.getArchivedConsignments(this.currentUser.PersonId).subscribe((res: any) => {
             this.archivedConsignments = res;
@@ -138,37 +119,6 @@ export class ConsignmentComponent implements OnInit {
         this.pagedItems = this.consignments.slice(this.pager.StartIndex, this.pager.EndIndex + 1);
     }
 
-    //filteringNumberOfConsignments(event: any) {
-    //    if (event.srcElement.value == 'option1'){
-    //        this.filteringSliceValue = 2;
-    //        this.maxSliceValue = this.filteringSliceValue;
-    //        this.showAllConsignmentsEnabled = false;
-    //    }
-    //    if (event.srcElement.value == 'option2') {
-    //        this.filteringSliceValue = 4;
-    //        this.maxSliceValue = this.filteringSliceValue;
-    //        this.showAllConsignmentsEnabled = false;
-    //    }
-    //    if (event.srcElement.value == 'option3') {
-    //        this.filteringSliceValue = 6;
-    //        this.maxSliceValue = this.filteringSliceValue;
-    //        this.showAllConsignmentsEnabled = false;
-    //    }
-    //    if (event.srcElement.value == 'option4') {
-    //        this.filteringSliceValue = 8;
-    //        this.maxSliceValue = this.filteringSliceValue;
-    //        this.showAllConsignmentsEnabled = false;
-    //    }
-    //    if (event.srcElement.value == 'option5') {
-    //        this.filteringSliceValue = 10;
-    //        this.maxSliceValue = this.filteringSliceValue;
-    //        this.showAllConsignmentsEnabled = false;
-    //    }
-    //    if (event.srcElement.value == 'option6') {
-    //        this.showAllConsignments();
-    //    }
-    //}
-
     showInvite(invite: Invitation) {
         console.log(invite);
     }
@@ -186,54 +136,4 @@ export class ConsignmentComponent implements OnInit {
 
             });
     }
-
-    //incrementSliceValue() {
-    //    if (this.maxSliceValue >= this.consignments.length)
-    //        return;
-
-    //    this.maxSliceValue += this.filteringSliceValue;
-    //    this.minSliceValue += this.filteringSliceValue;
-    //}
-
-    //incrementSliceValueTwice() {
-    //    this.maxSliceValue += (2 * this.filteringSliceValue);
-    //    this.minSliceValue += (2 * this.filteringSliceValue);
-    //}
-
-    //decrementSliceValue() {
-    //    if (this.minSliceValue >= this.filteringSliceValue) {
-    //        this.maxSliceValue -= this.filteringSliceValue;
-    //        this.minSliceValue -= this.filteringSliceValue;
-    //    }
-    //}
-
-    //decrementSliceValueTwice() {
-    //    this.maxSliceValue -= (2 * this.filteringSliceValue);
-    //    this.minSliceValue -= (2 * this.filteringSliceValue);
-    //}
-
-    //showAllConsignments() {
-    //    this.maxSliceValue = this.consignments.length;
-    //    this.minSliceValue = 0;
-
-    //    this.showAllConsignmentsEnabled = true;
-    //}
-
-    //maximizeSliceValue() {
-    //    if (this.consignments.length % this.filteringSliceValue > 0) {
-    //        this.maxSliceValue = this.consignments.length + (this.filteringSliceValue - (this.consignments.length % this.filteringSliceValue)); //Om antalet invitations inte är delbart på this.filteringSliceValue, ska den ändå visa rätt invites på sista sidan
-    //        this.minSliceValue = this.maxSliceValue - this.filteringSliceValue;
-    //    }
-    //    else {
-    //        this.maxSliceValue = this.consignments.length;
-    //        this.minSliceValue = this.maxSliceValue - this.filteringSliceValue;
-    //    }
-    //}
-
-    //resetSliceValue() {
-    //    this.maxSliceValue = this.filteringSliceValue;
-    //    this.minSliceValue = 0;
-
-    //    this.showAllConsignmentsEnabled = false;
-    //}
 }
