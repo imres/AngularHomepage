3e7f634e7b6d46a3087124a44e069b82372d3862
antonnystedt <32243913+antonnystedt@users.noreply.github.com>
2018-09-12 22:41:39 +0200

    Created a design for the Friends-tab under user-profile. Also changed the design for the AD-tab

---
+++ AngularHomepage/app/user-profile/friends.component.ts
@@ -0,0 +1,44 @@
+﻿import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, ChangeDetectionStrategy, ChangeDetectorRef, OnChanges  } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators, FormsModule } from '@angular/forms';
+import { DialogService } from "ng2-bootstrap-modal";
+import { User, Person } from '../_models/index';
+import { ConfirmComponent } from '../_dialog/confirm.component';
+import { AlertService, UserService, InvitationService, ToastrService } from '../_services/index';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'friends.component.html',
+    selector: 'friends',
+})
+
+export class FriendsComponent implements OnInit {
+    currentUser: Person;
+    confirmResult: boolean = null;
+
+    ngOnInit() {
+
+    }
+
+    constructor(private cd: ChangeDetectorRef,
+        private dialogService: DialogService,
+        private userService: UserService,
+        private alertService: AlertService,
+        private toastrService: ToastrService,
+    ) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    }
+
+    showConfirm(event: any) {
+        this.dialogService.addDialog(ConfirmComponent, {
+            title: 'Skicka inbjudan',
+            message: 'Bla bla confirm some action?'
+        })
+            .subscribe((isConfirmed) => {
+                //Get dialog result
+                this.confirmResult = isConfirmed;
+
+                this.toastrService.ShowToastr(isConfirmed, false, true, "Inbjudan skickades");
+
+            });
+    }
+}
