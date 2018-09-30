8991e0267d287763f3c2b2a7f96312247b342d6f
antonnystedt <32243913+antonnystedt@users.noreply.github.com>
2018-10-01 01:13:19 +0200

    Added a response if the search didnt get any hits

--- AngularHomepage/app/filter.pipe.ts
+++ AngularHomepage/app/filter.pipe.ts
@@ -6,11 +6,15 @@ export class FilterPipe implements PipeTransform {
     transform(items: any[], searchText: string): any[] {
         if (!items) return [];
         if (!searchText) return items;
-        return items.filter(it => {
+        let result = items.filter(it => {
             return it.firstName.toLowerCase().startsWith(searchText.toLowerCase())
                 || it.lastName.toLowerCase().startsWith(searchText.toLowerCase())
                 || (it.firstName.toLowerCase() + ' ' + it.lastName.toLowerCase()).startsWith(searchText.toLowerCase())
                 || it.token == searchText;
         });
+        if(result.length === 0) {
+            return [-1];
+        }
+        return result;
     }
 }
