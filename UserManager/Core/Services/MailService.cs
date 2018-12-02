16e690ceaaa49b2f4fd3986e92cc1ac8ad9f3233
imres <pontuswikberg_@hotmail.com>
2018-12-02 19:47:33 +0100

    SMTP gmail

---
+++ UserManager/Core/Services/MailService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace UserManager.Core.Services
+{
+    public static class MailService
+    {
+        public static void SendEmail()
+        {
+            var pontusmail = "";
+
+            string mailBodyhtml =
+                "<p>SMTP fungerar!!</p>";
+            var msg = new MailMessage(pontusmail, pontusmail, "Hello", mailBodyhtml);
+            msg.To.Add(new MailAddress(pontusmail));
+            msg.IsBodyHtml = true;
+            var smtpClient = new SmtpClient("smtp.gmail.com", 587); //if your from email address is "from@hotmail.com" then host should be "smtp.hotmail.com"
+            smtpClient.UseDefaultCredentials = true;
+            smtpClient.Credentials = new NetworkCredential(pontusmail, "password");
+            smtpClient.EnableSsl = true;
+            smtpClient.Send(msg);
+        }
+    }
+}
